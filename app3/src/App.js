4ccfe4ef82cd5fc391efe3e4e43b8ea7ff6deebd
Wellington Braga <wellington.braga@fcxlabs.com.br>
2024-11-15 17:43:11 -0300

    first commit

---
+++ app3/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        margin: "10px",
+        padding: "10px",
+        textAlign: "center",
+        backgroundColor: "pink",
+      }}
+    >
+      <h1>El El El Biel dá o anel</h1>
+    </div>
+  );
+};
+
+export default App;
